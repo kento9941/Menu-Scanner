5b1ee272e86b8fa659c13cc90bf935b2e7edf851
kawky008 <kento9941@gmail.com>
2025-09-20 17:48:33 +0930

    test(text_extractor): added test code and minor text_extractor updates

--- backend/src/text_extractor.py
+++ backend/src/text_extractor.py
@@ -5,10 +5,15 @@ class TextExtractor:
     def __init__(self, languages):
         self.__reader = easyocr.Reader(languages)
 
-    def extract_text(self, image_numpy_array: np.ndarray) -> set[str]:
+    def extract_text(self, image_numpy_array: np.ndarray) -> str:
         results = self.__reader.readtext(image_numpy_array, paragraph=False)
-        texts = set()
+
+        # convert into one string
+        menu_texts = ""
         for _, text, confidence in results:
-            if confidence > 0.8:
-                texts.add(text)
-        return texts
+            if confidence > 0.6:
+                menu_texts += text
+
+        # for JP text (伸ばし棒)
+        menu_texts = menu_texts.replace("-", "ー")
+        return menu_texts
