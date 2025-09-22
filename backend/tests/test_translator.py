360f6dcccb6e39558925dc1b0ba7e24c488dc340
kawky008 <kento9941@gmail.com>
2025-09-22 10:20:03 +0930

    feat: changed translate_text() to return detected language

--- backend/tests/test_translator.py
+++ backend/tests/test_translator.py
@@ -7,10 +7,16 @@ class TestTranslator(unittest.TestCase):
 
     def test_translate_EN_to_EN(self):
         en_text = "spaghetti bolognese"
-        translated_text = self.translator.translate_text(en_text, "en")
+        result = self.translator.translate_text(en_text, "en")
+        detected_language = result["detected_language"]
+        translated_text = result["translated_text"]
+        self.assertEqual("en", detected_language)
         self.assertEqual("spaghetti bolognese", translated_text)
 
     def test_translate_JP_to_EN(self):
         jp_text = "みそ汁"
-        translated_text = self.translator.translate_text(jp_text, "ja")
+        result = self.translator.translate_text(jp_text, "ja")
+        detected_language = result["detected_language"]
+        translated_text = result["translated_text"]
+        self.assertEqual("ja", detected_language)
         self.assertIn("Miso", translated_text)
