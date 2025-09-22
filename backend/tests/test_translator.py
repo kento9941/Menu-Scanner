2c94fca30ff0e62b32a70f74c41f11d36b60440d
kawky008 <kento9941@gmail.com>
2025-09-22 10:47:55 +0930

    test (translator): added tests for checking detected language

--- backend/tests/test_translator.py
+++ backend/tests/test_translator.py
@@ -6,17 +6,20 @@ class TestTranslator(unittest.TestCase):
         self.translator = Translator()
 
     def test_translate_EN_to_EN(self):
-        en_text = "spaghetti bolognese"
-        result = self.translator.translate_text(en_text, "en")
-        detected_language = result["detected_language"]
-        translated_text = result["translated_text"]
+        en_text = "pizza"
+        result = self.translator.translate_text(en_text)
+        detected_language = result.get("detected_language")
+        translated_text = result.get("translated_text")
+
+        self.assertEqual("pizza", translated_text)
         self.assertEqual("en", detected_language)
-        self.assertEqual("spaghetti bolognese", translated_text)
+        
 
     def test_translate_JP_to_EN(self):
         jp_text = "みそ汁"
-        result = self.translator.translate_text(jp_text, "ja")
-        detected_language = result["detected_language"]
-        translated_text = result["translated_text"]
+        result = self.translator.translate_text(jp_text)
+        detected_language = result.get("detected_language")
+        translated_text = result.get("translated_text")
+
         self.assertEqual("ja", detected_language)
         self.assertIn("Miso", translated_text)
