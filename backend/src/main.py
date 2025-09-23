cfd242b774e38b3b80206db52b067c126825fe12
kawky008 <kento9941@gmail.com>
2025-09-23 18:44:34 +0930

    fix (FastAPI): added exception handling for image numpy array conversion

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, Form, File, HTTPException
+from io import BytesIO
 import numpy as np
 from PIL import Image
 from src.menu_scanner import MenuScanner
@@ -15,5 +16,11 @@ async def upload_image(source_language: str = Form(...), image: UploadFile = Fil
     if not image.content_type.startswith("image/"):
         raise HTTPException(status_code=400, detail="File must be an image")
 
-    image_numpy_array = np.array(Image.open(image.file))
+    image_bytes = await image.read()
+    # catch invalid image format
+    try:
+        image_numpy_array = np.array(Image.open(BytesIO(image_bytes)))
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid image file")
+    
     return menu_scanner.scan_menu(image_numpy_array, source_language, "en")
