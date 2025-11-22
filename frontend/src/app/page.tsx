22fcd8d35a333df375596356657b9e86167e629f
kento9941 <kento9941@gmail.com>
2025-11-22 12:17:03 +1030

    fix: fixed button and dropdown layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
                 )}
               </div>
   
-              <div className="flex flex-col md:flex-row gap-4 md:gap-8 items-start justify-center mt-5">
+              <div className="flex flex-col md:flex-row items-center md:items-start justify-center gap-4 md:gap-8 mt-5">
                 <PhotoButtonWrapper imageSrc={imageSrc} setImageSrc={setImageSrc} />
                 <LanguageDropdown language={language} setLanguage={setLanguage} />
               </div>
